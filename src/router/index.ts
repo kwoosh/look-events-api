3d0c50cc0f991ec5f0407161b2aa190b1d56a41c
Andrew Pashinnik <tobirawork@gmail.com>
2018-06-14 05:11:23 +0300

    feat(router): add /events route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,20 @@
 import * as Router from 'koa-router'
+import { getEvents } from '../scraper/events'
+import { Tags } from '../scraper/uri-builder'
 
 const router = new Router()
+const v1 = router.prefix('/v1')
 
-router.get('/*', async ctx => {
-    ctx.body = 'IT events API powered by DOU.ua'
+v1.get('/events', async ctx => {
+    const tags: Tags = {}
+
+    if (ctx.query.city) tags.city = ctx.query.city
+    if (ctx.query.tag) tags.tag = ctx.query.tag
+
+    const events = await getEvents(tags)
+
+    ctx.response.set({ 'Content-Type': 'application/json' })
+    ctx.body = events
 })
 
 export default router
