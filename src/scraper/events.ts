867c8a2e12cd42dd104900d8da9068c03fb0a5bc
Andrew Pashinnik <tobirawork@gmail.com>
2018-06-14 18:17:24 +0300

    feat(scraper/events): add getAllEvents
    supports pagination

--- src/scraper/events.ts
+++ src/scraper/events.ts
@@ -1,71 +1,88 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import * as cheerio from 'cheerio'
 import { buildURI, Tags } from './uri-builder'
 import { cities, selectors } from './utils'
 
-export function getPage(tags?: Tags): Promise<CheerioStatic> {
-    return axios.get(buildURI(tags)).then(res => cheerio.load(res.data))
+export function getPage(tags?: Tags): Promise<CheerioStatic | void> {
+    return axios
+        .get(buildURI(tags))
+        .then(res => cheerio.load(res.data))
+        .catch((err: AxiosError) => undefined)
 }
 
-export function getEvents(tags?: Tags): Promise<Event[]> {
+export function getEventsForPage(tags?: Tags): Promise<Event[]> {
     return getPage(tags).then(page => {
         const events: Event[] = []
 
-        page(selectors.events).map((i, el) => {
-            const event = cheerio(el)
+        if (page) {
+            page(selectors.events).map((i, el) => {
+                const event = cheerio(el)
 
-            const id = Number(
-                event
+                const id = Number(
+                    event
+                        .children('h2.title')
+                        .children('a')
+                        .attr('href')
+                        .split('/')
+                        .find(str => !!Number(str))
+                )
+
+                const title = event
                     .children('h2.title')
-                    .children('a')
-                    .attr('href')
-                    .split('/')
-                    .find(str => !!Number(str))
-            )
+                    .text()
+                    .trim()
 
-            const title = event
-                .children('h2.title')
-                .text()
-                .trim()
+                const description = event
+                    .children('p.b-typo')
+                    .text()
+                    .trim()
 
-            const description = event
-                .children('p.b-typo')
-                .text()
-                .trim()
+                const topics = event
+                    .children('div.more')
+                    .find('a')
+                    .map((i, elem) => cheerio(elem).text())
+                    .get()
 
-            const topics = event
-                .children('div.more')
-                .find('a')
-                .map((i, elem) => cheerio(elem).text())
-                .get()
+                const time = event
+                    .children('div.when-and-where')
+                    .find('span.date')
+                    .text()
+                    .trim()
 
-            const time = event
-                .children('div.when-and-where')
-                .find('span.date')
-                .text()
-                .trim()
+                const price = event
+                    .children('div.when-and-where')
+                    .find('span')
+                    .not('.date')
+                    .text()
+                    .trim()
 
-            const price = event
-                .children('div.when-and-where')
-                .find('span')
-                .not('.date')
-                .text()
-                .trim()
+                const place = event
+                    .children('div.when-and-where')
+                    .text()
+                    .match(/[a-zA-Zа-яА-Я]+/g)
+                    .find(str => {
+                        if (str === 'Online') str = str.toLowerCase()
+                        return cities.includes(str)
+                    })
 
-            const place = event
-                .children('div.when-and-where')
-                .text()
-                .match(/[a-zA-Zа-яА-Я]+/g)
-                .find(str => {
-                    if (str === 'Online') str = str.toLowerCase()
-                    return cities.includes(str)
-                })
-
-            events.push({ id, title, time, price, place, description, topics })
-        })
+                events.push({ id, title, time, price, place, description, topics })
+            })
+        }
 
         return events
     })
+}
+
+export async function getAllEvents(tags?: Tags): Promise<Event[]> {
+    const totalEvents: Event[] = []
+
+    for (let i = 1; i < 50; i++) {
+        const eventsFromPage = await getEventsForPage({ ...tags, page: i })
+        if (!eventsFromPage.length) break
+        totalEvents.push(...eventsFromPage)
+    }
+
+    return totalEvents
 }
 
 export type Event = {
