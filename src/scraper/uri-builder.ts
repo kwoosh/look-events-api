e21b904ba917c7f2e004f340c12d8b5251c776eb
Andrew Pashinnik <tobirawork@gmail.com>
2018-06-14 18:16:25 +0300

    feat(scraper): buildURI
    add page tag for url building

--- src/scraper/uri-builder.ts
+++ src/scraper/uri-builder.ts
@@ -1,14 +1,18 @@
 export type Tags = {
     city?: string
     tag?: string
+    page?: number
 }
 
-export const buildURI = ({ city, tag }: Tags = {}): string => {
+export const buildURI = ({ city, tag, page }: Tags = {}): string => {
     let url = 'https://dou.ua/calendar/'
 
     if (city && !tag) url += `city/${city}/`
     if (!city && tag) url += `tags/${tag}/`
-    if (city && tag) url += `tags/${tag}/${city}`
+    if (city && tag) url += `tags/${tag}/${city}/`
+
+    if (page >= 2 && !city && !tag) url += `page-${page}/`
+    else if (page >= 2) url += `${page}/`
 
     return encodeURI(url)
 }
