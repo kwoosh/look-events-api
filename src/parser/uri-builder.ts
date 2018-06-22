ef885f23d425ff1ed9cb905292db033356328baa
Andrew Pashinnik <tobirawork@gmail.com>
2018-06-22 20:56:58 +0300

    refactor(parser/uri-builder): remove unused params

--- src/parser/uri-builder.ts
+++ src/parser/uri-builder.ts
@@ -1,22 +1,14 @@
 export type Params = {
     id?: number
-    city?: string
-    tag?: string
     page?: number
     fromArchive?: boolean
 }
 
-export const buildURI = ({ city, tag, page, fromArchive, id }: Params = { fromArchive: false }): string => {
+export const buildURI = ({ page, fromArchive, id }: Params = { fromArchive: false }): string => {
     let url = fromArchive ? 'archive/' : ''
 
-    if (city && !tag) url += `city/${city}/`
-    if (!city && tag) url += `tags/${tag}/`
-    if (city && tag) url += `tags/${tag}/${city}/`
-
-    if (page >= 2 && !city && !tag) url += `page-${page}/`
-    else if (page >= 2) url += `${page}/`
-
-    if (id) url = `${id}`
+    if (page >= 2) url += `${page}/`
+    if (id) url = `${id}/`
 
     return encodeURI(`https://dou.ua/calendar/${url}`)
 }
