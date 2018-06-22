3045226ae0a47d7500c5838d2c5e3aacb97d0712
Andrew Pashinnik <tobirawork@gmail.com>
2018-06-22 11:58:07 +0300

    feat(parser/uri-builder): add id to tags param

--- src/parser/uri-builder.ts
+++ src/parser/uri-builder.ts
@@ -1,11 +1,12 @@
 export type Tags = {
+    id?: number
     city?: string
     tag?: string
     page?: number
     fromArchive?: boolean
 }
 
-export const buildURI = ({ city, tag, page, fromArchive }: Tags = { fromArchive: false }): string => {
+export const buildURI = ({ city, tag, page, fromArchive, id }: Tags = { fromArchive: false }): string => {
     let url = fromArchive ? 'archive/' : ''
 
     if (city && !tag) url += `city/${city}/`
@@ -14,6 +15,8 @@ export const buildURI = ({ city, tag, page, fromArchive }: Tags = { fromArchive:
 
     if (page >= 2 && !city && !tag) url += `page-${page}/`
     else if (page >= 2) url += `${page}/`
+
+    if (id) url = `${id}`
 
     return encodeURI(`https://dou.ua/calendar/${url}`)
 }
