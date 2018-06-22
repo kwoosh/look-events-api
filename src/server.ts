40f18cc3aa61e94d9dc8adbf0ea6bff05a131b94
Andrew Pashinnik <tobirawork@gmail.com>
2018-06-22 21:19:34 +0300

    feat(srver): log if development

--- src/server.ts
+++ src/server.ts
@@ -15,4 +15,4 @@ app.use((ctx, next) => {
 
 app.listen(process.env.PORT || 3000)
 
-console.log('Server running on port http://localhost:3000')
+if (process.env.NODE_ENV === 'development') console.log('Server running on port http://localhost:3000')
